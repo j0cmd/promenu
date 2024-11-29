dd1e03b75c6b895a59e7415bf9a57d1fe79ba1ca
j0cmd <jomaiaaraujo@gmail.com>
2024-11-29 15:13:15 -0300

    sequecia cap2

--- cap2.py
+++ cap2.py
@@ -9,22 +9,33 @@ def visao_geral():
     return conteudo
 
 
+def compeesoesList():
+    print('veja o codigo para melhor compeessao')
+    print('Esse exemplo mostra uma compeessao de lista')
+    symbols = '$¢£¥€¤'
+    code = [ord(symbol) for symbol in symbols]
+    print(code)
+
+# acima todo o conteudo estudado
 def menu():
     m = [
         (print('visao geral')),
-        (print('sequenci...'))
+        (print('compressao')),
+        (print('sequenci...')),
     ]
     return m
 
 
 def mainCap2():
     visaoGeral = visao_geral()
-
+    compressao = compeesoesList()
     # menu de organização
     menu()
     msg = input('opçoes ')
     match msg:
         case 'visao geral':
             print(visaoGeral)
+        case 'comprssao':
+            print(compressao)
         case _:
             print('fim')
